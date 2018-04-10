f2ac631e2ec81e3957d38bba82f3be8f77193447
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-04-10 21:25:54 +0100

    Use say instead of print

--- perlmaven.com/gettingstarted.pl
+++ perlmaven.com/gettingstarted.pl
@@ -1,7 +1,8 @@
 # https://perlmaven.com/installing-perl-and-getting-started
 # no shebang in this one
 use 5.010;
+# Pragmas that help catch bugs
 use strict;
 use warnings;
 
-print "Hello world!\n";
+say "Hello world!";
